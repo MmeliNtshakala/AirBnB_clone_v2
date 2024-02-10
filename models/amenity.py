bd76f9f6ea27d7ca057fa4b94f7738f71bc5a348
MmeliNtshakala <mmeliroot@gmail.com>
2024-02-10 05:20:20 -0800

    amenity 1

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Amenity class comes from base model
+"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+        Class Amenity
+    """
+
+    name = ""
