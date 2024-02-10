2552db416b9ef6449e24c865e16f98a2cb7242e7
MmeliNtshakala <mmeliroot@gmail.com>
2024-02-10 04:49:20 -0800

    state

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    State class from BaseModel
+"""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+        CLASS STATE
+    """
+    name = ""
