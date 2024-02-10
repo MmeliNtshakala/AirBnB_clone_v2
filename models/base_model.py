8aa7a180c623a1d57ad99605d8ccdaf5d10d8d5b
MmeliNtshakala <mmeliroot@gmail.com>
2024-02-09 23:20:01 -0800

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,35 +4,58 @@
 """
 
 import uuid
-from datetime import datetime
-
+import datetime
+import models
 
 class Basemodel:
     """
         Defines all common attributes for other class
     """
 
-    def __init__(self):
-        seld.id = str(uuid.uuid4())
+    def __init__(self, *args, **kwargs):
+        if (len(kwargs) == 0):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
+         else:
+            kwargs["created_at"] = datetime.datetime.strptime(
+                kwargs["created_at"],
+                "%Y-%m-%dT%H:%M:%S.%f"
+            )
+            kwargs["updated_at"] = datetime.datetime.strptime(
+                kwargs["updated_at"],
+                "%Y-%m-%dT%H:%M:%S.%f"
+            )
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
 
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
 
     def save(self):
         """
             Updates the self updatee with the current date time
         """
         self.updated_at = datetime.utcnow()
+        #CALL THE SAVE
+        models.storage.save()
 
     def to_dict(self):
         """
             Turning an instance to Passing self to Dictionary
         """
-        inst_dict = self.__dict__.copy()
+        dcti = self.__dict__.copy()
         #access and manipulate project atributes
-        inst_dict["__class__"] = self.__class__.__name__
-        inst_dict["created_at"] = self.created_at.isoformat()
-        inst_dict["updated_at"] = self.updated_at.isoformat()
+        dcti["__class__"] = self.__class__.__name__
+        dcti["created_at"] = self.created_at.isoformat()
+        dcti["updated_at"] = self.updated_at.isoformat()
 
-        return inst_dict
+        return dcti
+
+    def __str__(self):
+        """
+            Returns everything in base model as string
+        """
+        string = "[{}] ({}) {}"
+        return string.format(type(self).__name__, self.id, self.__dict__)
 
