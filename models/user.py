f722e2ae8d6755b7e3db163fcbb0fceec9cc71db
MmeliNtshakala <mmeliroot@gmail.com>
2024-02-10 04:45:43 -0800

    User

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Class user inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """
+        Class user
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last name = ""
